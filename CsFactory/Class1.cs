6f2b20cc4d6975a50e81909c2191ff8da6473416
YuAn <anm821@gmail.com>
2024-03-01 17:26:26 +0800

    try do create object and create new project of CsFactory.sln for End-To-End Test

--- CsFactory/Class1.cs
+++ CsFactory/Class1.cs
@@ -1,5 +1,40 @@
-﻿namespace CsFactory;
+﻿using System.Reflection;
+
+namespace CsFactory;
 
 public class Class1
 {
+    public class CsFactory
+    {
+        public T Create<T>() where T : new()
+        {
+            var instance = new T();
+
+            foreach (var property in typeof(T).GetProperties())
+            {
+                var defaultValue = GetDefaultValue(property);
+                property.SetValue(instance, defaultValue);
+            }
+
+            return instance;
+        }
+
+        private object? GetDefaultValue(PropertyInfo propertyInfo)
+        {
+            var number = 0;
+            var type = propertyInfo.PropertyType;
+
+            if (type == typeof(int))
+            {
+                return number;
+            }
+
+            if (type == typeof(string))
+            {
+                return propertyInfo.Name + "#number";
+            }
+
+            return Activator.CreateInstance(type);
+        }
+    }
 }
